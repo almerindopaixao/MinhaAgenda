bf75779ae997628c676ac1a3030279b07c722cb7
Angelica-souza <angelicatecinfor@gmail.com>
2023-06-25 11:48:57 -0300

    feat: add middlewares

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,9 @@
+export const authMidleware = (req, res, next) => {
+    if (!req.session.user) {
+        req.session.save(() => res.redirect('/auth/login'));
+        return;
+    }
+
+    res.locals.user = req.session.user;
+    next();
+}
